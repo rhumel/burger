75bfbeb632b4ae32f8eac444cccf9257ff27d1cb
rhumel <rhumel@hotmail.com>
2018-09-12 22:47:04 -0400

    add more code

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,14 +1,63 @@
 // inport files
 var express = require('express');
-var burger = require('burger.js');
+var burger = require('../models/burger.js');
 
+var router = express.Router();
 
-// create the 'router' the app and export the router at end
+// Create all our routes and set up logic within those routes where required.
 
+router.get("/burgers", function(req, res) {
+    
+    burger.all(function(data) {
+      var hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
-// export router
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "name", "eaten"
+  ], [
+    req.body.name, req.body.eaten
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  cat.update({
+    eaten: req.body.eaten
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
 module.exports = router;
-
-
-
 
---
+++ models/burger.js
@@ -0,0 +1,31 @@
+// INport orm.js
+var orm = require('../config/orm');
+
+var burger = {
+    all: function(cb) {
+      orm.all("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.create("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.update("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
+
+  
+
+// export this file
+module.exports = burger.js;
