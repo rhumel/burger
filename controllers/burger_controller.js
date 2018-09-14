546ffa206e0c0f9b90d925406da78d167d7ed1d3
rhumel <rhumel@hotmail.com>
2018-09-14 16:11:32 -0400

    heroku test

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -6,8 +6,10 @@ var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 
-router.get("/burgers", function(req, res) {
-    
+// router.get("/burgers", function(req, res) {
+  router.get("/burgers/rhumelburger", function(req, res) {
+
+  
     burger.all(function(data) {
       var hbsObject = {
         burgers: data
